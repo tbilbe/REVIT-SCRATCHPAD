b1a3b9f6dfcd7e31cd25937904ebf22d19585cd4
Thomas <tbilbe@gmail.com>
2019-03-21 12:10:49 +0000

    more efficient ifs and production ready

--- cableTrayInSpace.py
+++ cableTrayInSpace.py
@@ -1,11 +1,19 @@
-SpaceCollector = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_MEPSpaces).WhereElementIsNotElementType().ToElements()
+from Autodesk.Revit.DB import *
+from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory
+doc = __revit__.ActiveUIDocument.Document
 
+__doc__ = "Adds the space name and space number to the BBK_MEP_LOCATION parameter."
+__title__ = '''Cable Tray\n in Space'''
+__author__ = "Tom Bilbe"
+
+
+SpaceCollector = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_MEPSpaces).WhereElementIsNotElementType().ToElements()
 cTrayCollector = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_CableTray).WhereElementIsNotElementType().ToElements()
+
 def midpoint (mepObj):
     p1 = mepObj.Location.Curve.GetEndPoint(0)
     p2 = mepObj.Location.Curve.GetEndPoint(1)
     midpoint = XYZ((p1[0]+p2[0])/2, (p1[1]+p2[1])/2, (p1[2]+p2[2])/2)
-    #print(midpoint)
     return midpoint # Returns (xx.abc, yy.def, zz.ghi)
 
 def dropPointInRoom (point):
@@ -29,21 +37,15 @@ t = Transaction(doc, 'CableTray Space naming')
 t.Start()
 
 for space in SpaceCollector:
-	spaceNumber = space.LookupParameter('Number')
-	spaceName = space.LookupParameter('Name')
+    spaceNumber = space.LookupParameter('Number')
+    spaceName = space.LookupParameter('Name')
 	for cTray in cTrayCollector:
-
 		setBBKLocation = cTray.LookupParameter('BBK_MEP_LOCATION')
-		# if setBBKLocation.AsString() != None:
-		# 	if spaceNumber.AsString() == setBBKLocation.AsString():
-		# 		print('already set correct number: ' + str(spaceNumber.AsString()))
-		# 		continue
-		#     setBBKLocation.Set('')
-		if setBBKLocation.AsString() == '':
-			a = space.IsPointInSpace(dropPointInRoom(midpoint(cTray)))
-			b = wholeDuctInRoom(space, cTray)
-			if  a and b:
-				setBBKLocation.Set(spaceNumber.AsString())
-			if a and not b:
-				setBBKLocation.Set(str(spaceNumber.AsString()) + ': Duct in two spaces')
+        if setBBKLocation.AsString() == '':
+            a = space.IsPointInSpace(dropPointInRoom(midpoint(cTray)))
+            b = wholeDuctInRoom(space, cTray) 
+            if a and b:
+                setBBKLocation.Set(spaceNumber.AsString())
+            if a and not b:
+                setBBKLocation.Set(str(spaceNumber.AsString()) + ': Duct in two spaces')
 t.Commit()
