5e57244f97f810bf23768f0fe3d89d3ab20bb368
Thomas <tbilbe@gmail.com>
2019-03-21 12:09:39 +0000

    more efficient ifs, update to production

--- cableTrayFtngsInSpace.py
+++ cableTrayFtngsInSpace.py
@@ -1,3 +1,11 @@
+from Autodesk.Revit.DB import *
+from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory
+doc = __revit__.ActiveUIDocument.Document
+
+__doc__ = "Adds the space name and space number to the BBK_MEP_LOCATION parameter."
+__title__ = '''Cable Fittings\n in Space'''
+__author__ = "Tom Bilbe"
+
 SpaceCollector = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_MEPSpaces).WhereElementIsNotElementType().ToElements()
 fittingsCollector = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_CableTrayFitting).WhereElementIsNotElementType().ToElements()
 
@@ -8,17 +16,16 @@ def dropPointInRoom (point):
     inRoomMidpoint = XYZ(point[0], point[1], lowerZPoint)
     return inRoomMidpoint
 
-t = Transaction(doc, 'Lets fucking do this again')
+t = Transaction(doc, 'Cable Fittings in Space')
 t.Start()
 
 for space in SpaceCollector:
+    spaceNumber = space.LookupParameter('Number')
+    spaceName = space.LookupParameter('Name')
     for cTrayFtngs in fittingsCollector:
         cTrayFtngsInsertPoint = cTrayFtngs.Location.Point
-        if setBBKLoc.AsString() == None:
-            if space.IsPointInSpace(dropPointInRoom(cTrayFtngsInsertPoint)):
-                spaceNumber = space.LookupParameter('Number')
-                spaceName = space.LookupParameter('Name')
-                setBBKLoc = cTray.LookupParameter('BBK_MEP_LOCATION')
-                setBBKLoc.Set(spaceNumber.AsString())
+        setBBKLoc = cTrayFtngs.LookupParameter('BBK_MEP_LOCATION')
+        if space.IsPointInSpace(dropPointInRoom(cTrayFtngsInsertPoint)):
+            setBBKLoc.Set(str(spaceNumber.AsString())+': '+str(spaceName.AsString()))
 
 t.Commit()
